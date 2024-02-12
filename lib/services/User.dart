e05df7b3fc9e5d75f951acd7cf09454be13f683c
mohammed haydar <mohammedhaydar97@gmail.com>
2024-02-12 19:30:49 +0200

    added http

--- lib/services/User.dart
+++ lib/services/User.dart
@@ -1,28 +1,28 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UserApis {
-  final Dio _dio = Dio();
-
   Future<bool> loginUsingEmail(String email, String password) async {
     try {
-      final response = await _dio.post(
-        'http://192.168.1.106:8000/api/users/login',
-        data: {
+      final response = await http.post(
+        Uri.parse('http://192.168.1.106:8000/api/users/login'),
+        body: {
           'email': email,
           'password': password,
         },
       );
 
       if (response.statusCode == 200) {
-        String token = response.data['token'];
+        final jsonData = json.decode(response.body);
+        String token = jsonData['token'];
         await saveToken(token);
         print('Login successful!');
-        print(response.data);
+        print(jsonData);
         return true;
       } else {
         print('------------------------------Error: ${response.statusCode}');
-        print(response.data);
+        print(response.body);
         return false;
       }
     } catch (err) {
@@ -33,7 +33,7 @@ class UserApis {
 
   Future<void> saveToken(String token) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
-    var tokenString = await prefs.setString('jwt_token', token);
-    print('--------------------------------------------$tokenString');
+    await prefs.setString('jwt_token', token);
+    print('Token saved: $token');
   }
 }
